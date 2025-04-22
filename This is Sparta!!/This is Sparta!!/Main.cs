9b992ce7a8da4c9288728e9cbfe07662a3a75721
ChunBae20 <drchunbae@gmail.com>
2025-04-23 04:09:59 +0900

    Update Main.cs
    인벤토리 -메인 -스테이터스 연결완료했습니다.
    ♥는 수정한부분입니다.
    잘보이게 하트여러개써놨습니다.
    주석 지저분하게 달았습니다 
    나중에 제대로 깔끔히 수정하겠습니다.

--- This is Sparta!!/This is Sparta!!/Main.cs
+++ This is Sparta!!/This is Sparta!!/Main.cs
@@ -1,39 +1,180 @@
-﻿using System;
-using System.Reflection;
-using static System.Runtime.InteropServices.JavaScript.JSType;
+//메인
 
-namespace This_is_Sparta__
+using System;
+using System.Runtime.InteropServices;
+using This_is_Sparta__;
+using OnlytestTRPG; //나만 추가
+
+
+// 자식 : 부모 힐아이템 인벤토리 다수정
+public class MainSpace                        //♥♥♥원래는 program이엇던것♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥
 {
-    internal class MainSpace // 메인에 Ctrl + C, Ctrl + V
+
+
+    static void Main(string[] args)
     {
-        HealItem healItem = new HealItem();
-        Inventory inventory = new Inventory();
-        Quest quest = new Quest();
-        static void Main(string[] arg)
-        {
-            MainSpace mainSpace = new MainSpace();
-            mainSpace.MainMenu();
-        }
 
-        public void MainMenu()
+
+        MainMenu();                       //♥♥♥원래는StartScene()♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥
+
+
+
+    }
+
+    public static void MainMenu()                 //♥♥♥원래는 public static void StartScene()♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥
+    {
+
+
+
+
+        while (true)
         {
-            inventory.InventoryUI();
-            healItem.Heal();
-            quest.QuestOption();
-        }
-        public int Input(int min, int max)
-        {
-            while (true)
+
+
+
+
+            Console.Clear();
+            Console.WriteLine(" 스파르타 던전에 오신 여러분 환영합니다.");
+            Console.WriteLine(" 이제 전투를 시작할 수 있습니다. \n\n\n");
+
+            Console.WriteLine("1. 상태 보기");
+            Console.WriteLine("2. 인벤토리");
+            Console.WriteLine("3. 회복 아이템");
+            Console.WriteLine("4. 전투 시작");
+            Console.WriteLine("원하시는 행동을 입력해주세요.....");
+
+            string Maininput = Console.ReadLine();
+
+            switch (Maininput)
             {
-                string input = Console.ReadLine() ?? "";
-                bool IsSelect = int.TryParse(input, out int number);
+                case "1":
+                    StatusScene();
+                    break;
+                case "2":
+                    inven.InventoryUI();
+                    break;
+                case "3":
+                    HealItem();
+                    return;
+                case "4":
+                    BattleScene();
+                    return;
+                case "team5NP":
+                    TeamMembers();
+                    break;
 
-                if (IsSelect && min <= number && max >= number)
-                {
-                    return number;
-                }
-                Console.WriteLine("잘못된 입력입니다");
+                default:
+                    Console.WriteLine("잘못된 입력입니다. 아무 키나 누르면 다시 선택화면으로 돌아갑니다.");
+                    Console.ReadKey();
+                    break;
             }
         }
     }
+
+
+    public static Inventory inven = new Inventory(); //Inventory의 클래스를 가져와서 inven이라는 새로운 인스턴스 생성한다.
+    public static Status status = new Status();      //Status의 클래스를 가져와서 status라는 새로운 인스턴스를 생성한다.
+    static void StatusScene()
+    {
+
+        //기본 체력부
+        int basicstr = status.basicSTR;
+        int basicdef = status.basicDEF;
+        int basicHP = status.basicHP;
+        int basicgold = status.basicGold;
+
+        //추가부
+        //int weaponSTR  ;//nowEquipSTR; //weaponSTR은 nowEquipSTR라는 Inventory의클래스 내부 변수에서 가져온다.현재 장착한 무기
+        //int weaponDEF ; //weaponDEF은 nowEquipDEF라는 Inventory의클래스 내부 변수에서 가져온다.현재 장착한 방어구
+
+        
+        int nowHP = status.HP;             //nowHP는 HP라는 Status의 클래스 내부 변수에서 가져온다. 현재체력
+        int nowGold = status.Gold;         //nowGold는 Gold라는 Status의클래스 내부 변수에서 가져온다. 현재보유골드.
+
+        Console.Clear();
+        Console.WriteLine(" 캐릭터의 정보가 표시됩니다.\n\n\n");
+        Console.WriteLine("공격력 : " + basicstr + " (+ " + status.nowEquipSTR + ")");     // 기본 공력력10 (+장비 공격력)
+        Console.WriteLine("방어력 : " + basicdef + " (+ " + status.nowEquipDEF + ")");      // 기본 방어력5  (+장비 방어력)
+        Console.WriteLine("체력 :" + basicHP + " ( " + nowHP + ")");             // 기본 체력100  (+장비 체력)
+        Console.WriteLine("Gold : " + basicHP + " ( " + nowGold + ")");            // 기본 골드
+
+    backagain:
+        Console.WriteLine("\n\n\n원하는 행동을 입력하세요");
+        Console.WriteLine(" 0. 나가기\n\n\n");
+
+        string gotoStartScene = "0";
+        string gotoexit = Console.ReadLine();
+        if (gotoexit == gotoStartScene)
+        {
+            MainMenu();                          ///♥♥♥♥♥♥♥♥♥♥♥♥원래는 StartScene();엿던것♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥
+        }
+        else
+        {
+            goto backagain;
+        }
+
+
+        Console.WriteLine("상태보기 입니다. \n아무 키나 누르면 돌아갑니다.");
+        Console.ReadKey();
+    }
+    public static int Input(int min, int max)   //♥♥♥♥♥♥♥♥♥♥♥♥ 인벤토리에서 Input값을 못불러옴 그래서 static 썼음
+    {
+        while (true)
+        {
+            string input = Console.ReadLine() ?? "";
+            bool IsSelect = int.TryParse(input, out int number);
+
+            if (IsSelect && min <= number && max >= number)
+            {
+                return number;
+            }
+            Console.WriteLine("잘못된 입력입니다");
+        }
+    }
+        static void InventoryScene()
+    {
+        Console.Clear();
+        Console.WriteLine("인벤토리화면입니다. \n아무 키누르면 프로그램 종료");
+
+        Console.ReadKey();
+    }
+
+    static void HealItem()
+    {
+        Console.Clear();
+        Console.WriteLine("회복 아이템 화면입니다. \n아무 키누르면 프로그램 종료");
+
+        Console.ReadKey();
+    }
+
+    static void BattleScene()
+    {
+        Console.Clear();
+        Console.WriteLine("전투화면입니다. \n아무 키누르면 프로그램 종료");
+        Console.ReadKey();
+    }
+
+    static void TeamMembers()
+    {
+
+        Console.Clear();
+
+        Console.WriteLine(" 테크니컬 리더 님 감사합니다. ");
+        Console.WriteLine(" 님 감사합니다. ");
+        Console.WriteLine(" 님 감사합니다.");
+        Console.WriteLine(" 님 감사합니다.");
+        Console.WriteLine(" 님 갑사합니다 ");
+
+        Console.WriteLine(" 1조의 팀원들입니다. \n아무 키누르면 프로그램 종료");
+        Console.ReadKey();
+
+
+
+    }
 }
+
+
+
+
+
+
